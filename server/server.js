20a90f37e325e8114ae88fcecfb6717db4f431e1
adamromero <adamromero89@gmail.com>
2022-08-02 16:27:51 -0700

    Build for production

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
 import projectRoutes from "./routes/projectRoutes.js";
@@ -23,6 +25,21 @@ app.use("/api/allUsers", allUsersRoutes);
 app.use("/api/ticket", ticketRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/aws", awsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+   const __filename = fileURLToPath(import.meta.url);
+   const __dirname = path.dirname(__filename);
+
+   app.use(express.static(path.join(__dirname, "../client/build")));
+
+   app.get("*", (req, res) =>
+      res.sendFile(
+         path.resolve(__dirname, "../", "client", "build", "index.html")
+      )
+   );
+} else {
+   app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
